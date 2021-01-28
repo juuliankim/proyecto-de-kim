d2d151efbd2dcd86c7c7288c8d5df6754684b212
julian kim <juuliank@gmail.com>
2021-01-27 22:10:10 -0300

    navbar

---
+++ src/components/navbar/index.jsx
@@ -0,0 +1,32 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const NavbarComponent = () => {
+    return (
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Bella</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                        <a class="nav-link active" href="#">Inicio</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" href="#">Tienda</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" href="#">Rebajas</a>
+                        </li>
+                        <li class="nav-item">
+                        <a class="nav-link active" href="#">Contacto</a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+    )
+}
+
+export default NavbarComponent;
