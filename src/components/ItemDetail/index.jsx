9459c054d1ac17a43e2bac812ec0113dab64f613
julian kim <juuliank@gmail.com>
2021-02-16 00:36:38 -0300

    1602202101

--- src/components/ItemDetail/index.jsx
+++ src/components/ItemDetail/index.jsx
@@ -1,26 +1,38 @@
 import { useParams } from "react-router-dom"
 import { useEffect, useState } from 'react'
 import listaProducto from "../../mocks/listaProducto"
+import ItemCountComponent from "../ItemCount"
 
 const ItemDetailComponent = () => {
 
     const {nombreProducto} = useParams()
-    const [precio, setPrecio] = useState([])
+    const [precios, setPrecio] = useState([])
+    const [url, setUrl] = useState([])
+    const [descripcion, setDescripcion] = useState([])
+    const [stock, setStock] = useState([])
 
     useEffect(() => {
-        let precios = listaProducto.filter((element) => {
-            if(nombreProducto == element.nombre) {
-                return element.precio
-            }
+        listaProducto.filter((element) => {
+            return element.name === nombreProducto
         })
-        setPrecio(precios[0].precio)
+        setPrecio(listaProducto[0].precio)
+        setUrl(listaProducto[0].url)
+        setDescripcion(listaProducto[0].descripcion)
+        setStock(listaProducto[0].stock)
     }, [nombreProducto])
 
     return (
+        <>
         <div>
             <h2>{nombreProducto}</h2>
-            <h3>{precio.precio}</h3>
+            <img src={url} alt="" width="400" height="550"/>
+            <p>Precio: {precios}</p>
+            <p>Descripcion: {descripcion}</p>
         </div>
+        <div>
+        <ItemCountComponent descripcion={"tenemos en Stock"} stock={stock} inicial={1}/>
+        </div>
+        </>
     )
 }
 
